46d436196f73da04a9e7a6529ec7200665ccbfdc
Hemant Verma <vermahemant837@gmail.com>
2023-07-26 17:05:15 +0530

    next commit

---
+++ getting_startes.js
@@ -0,0 +1,73 @@
+// let headerTitle = document.getElementById("header-title");
+
+// headerTitle.textContent = "Sharpener";
+// headerTitle.innerText = "Platform";
+
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+
+// headerTitle.innerHTML = "<h3>Hello<h3>";
+
+// headerTitle.style.color = "blue";
+// headerTitle.style.fontSize = "40px";
+
+// let header = document.getElementById("main=header");
+
+// let items = document.getElementsByClassName("list-group-item");
+
+// items[0].style.fontSize = "20px";
+// items[1].innerText = "Hello 2";
+// items[1].style.color = "red";
+// items[2].style.fontWeight = "bold";
+// items[3].style.backgroundColor = "pink";
+
+// for (let item of items) {
+//   item.style.backgroundColor = "#f4f4f4";
+// }
+
+// let li = document.getElementsByTagName("li");
+
+// li[0].style.fontSize = "20px";
+// li[1].innerText = "Hello 2";
+// li[1].style.color = "red";
+// li[2].style.fontWeight = "bold";
+// li[3].style.backgroundColor = "pink";
+
+// for (let ele of li) {
+//   ele.style.backgroundColor = "#f4f4f4";
+// }
+
+// let header = document.querySelector("#main-header");
+
+// header.style.borderBottom = "4px solid brown";
+
+// let input = document.querySelector("input");
+// input.value = "Hello Sharpeners";
+
+// let submit = document.querySelector("input[type='submit']");
+
+// submit.value = "Send";
+
+// let item = document.querySelector(".list-group-item");
+
+// item.style.color = "red";
+
+// let lastItem = document.querySelector(".list-group-item:last-child");
+
+// lastItem.style.color = "blue";
+
+// let titles = document.querySelectorAll(".title");
+
+// console.log(titles);
+
+// let even = document.querySelectorAll("li:nth-child(even)");
+
+// for (item of even) {
+//   item.style.backgroundColor = "green";
+// }
+
+// let odd = document.querySelectorAll("li:nth-child(odd)");
+
+// for (item of odd) {
+//   item.style.backgroundColor = "orange";
+// }
