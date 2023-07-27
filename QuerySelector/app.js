733cc31a2632ea60c8817366c1f07ad0de546cc7
Hemant Verma <vermahemant837@gmail.com>
2023-07-27 12:56:37 +0530

    third commit

---
+++ QuerySelector/app.js
@@ -0,0 +1,29 @@
+// QuerySelector
+
+// First Task
+
+// let secondItem = document.querySelector(".list-group-item:nth-child(2)");
+
+// secondItem.style.backgroundColor = "green";
+
+// Second Task
+
+// let thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+
+// thirdItem.style.display = "block";
+
+// QuerySelectorALL
+
+// First Task
+
+// let secondItem = document.querySelectorAll("li");
+
+// secondItem[1].style.color = 'green';
+
+// Second Task
+
+// let odd = document.querySelectorAll("li:nth-child(odd)");
+
+// for (ele of odd) {
+//   ele.style.backgroundColor = "green";
+// }
