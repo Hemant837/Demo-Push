6aed11c5d56d5c8f7cc01a6c5ffdef414729fb49
Hemant Verma <vermahemant837@gmail.com>
2023-07-27 10:42:29 +0530

    today's first commit

---
+++ Classname/byClassname.js
@@ -0,0 +1,9 @@
+let item3 = document.getElementsByClassName("list-group-item")[2];
+
+item3.style.backgroundColor = "green";
+
+let items = document.getElementsByClassName("list-group-item");
+
+for (let item of items) {
+  item.style.fontWeight = "bold";
+}
